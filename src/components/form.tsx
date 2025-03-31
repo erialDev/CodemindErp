4d8d17e53ffc84188e58018a4e52aaeaf8147347
erialdev <iclouderick@outlook.com>
2025-03-30 22:00:12 -0500

    Responsive y tipo de fuente

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -7,27 +7,61 @@ const CustomForm: React.FC = () => {
     const [password, setPassword] = useState("");
 
     return (
-        <div className={`container-fluid ${stylesform.container}`}>
-            <div className="row">
-                {/* Sección izquierda con imagen */}
-                <section className={`col-md-6 p-0 d-none d-md-block ${stylesform.cardIzq}`} >
-                    <Image
-                        width={0}
-                        height={0}
-                        src="/image.jpg"
-                        alt="Imagen"
-                        layout="fill"
-                        objectFit="cover"
-                    />
-                </section>
+        <div className={stylesform.card_principal}>
 
-                {/* Sección derecha con contenido */}
-                <section className="col-12 col-md-6 d-flex justify-content-center align-items-center" style={{ height: "100vh", zIndex: 2 }}>
-                    <div className="text-center">
-                        <h2>Sección derecha</h2>
+            <section className={stylesform.card_izq}>
+                <div className={stylesform.card_text}>
+                    <p>Bienvenido</p>
+                </div>
+                <div className={stylesform.card_image}>
+                    <Image
+                        src="/logoCodemind.svg"
+                        alt="logo"
+                        width={400}
+                        height={300}
+                        className={stylesform.image}
+                    />
+                </div>
+                <div className={stylesform.card_register}>
+                    <p>
+                        ¿No tienes una cuenta?
+                    </p>
+
+                    <a><strong> Registrate</strong></a>
+                </div>
+            </section>
+
+            <section className={stylesform.card_der}>
+                <div className={stylesform.card_form}>
+                    <div className={stylesform.card_login}>
+                        <h1>Iniciar Sesión</h1>
                     </div>
-                </section>
-            </div>
+                    <form className={stylesform.form} autoComplete="on">
+                        <label htmlFor="">Email o Usuario:</label>
+                        <input type="email" name="" id="" placeholder="Ingrese su correo" autoComplete="email" />
+
+                        <label htmlFor="">Contraseña</label>
+                        <input type="password" name="" id="" placeholder="Ingrese su contraseña" autoComplete="current-password" />
+                        <div className={stylesform.card_opciones}>
+                            <div className={stylesform.card_chk}>
+                                <input type="checkbox" id="chk" defaultChecked className={stylesform.chk} />
+                                <label htmlFor="chk" className={stylesform.chklabel}></label>
+                                <span className={stylesform.chktext}>Recuerdame</span>
+                            </div>
+                        </div>
+                        <div className={stylesform.card_button} ><button type="submit" className={stylesform.button_login}>Ingresar</button></div>
+                        <div className={stylesform.cardRecordPassword}>
+                            <a
+                                href="#"
+                                className={stylesform.card_opcion}
+                            >
+                                Olvidé mi contraseña
+                            </a>
+                        </div>
+                    </form>
+                </div>
+            </section>
+
         </div>
     );
 };
